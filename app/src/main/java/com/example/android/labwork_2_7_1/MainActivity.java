f6b175d0027b045ff1ae9438c31075a870a057ec
AlexYach <Alexyach1973@gmail.com>
2021-12-01 22:11:55 +0200

    Start LabWork 2.7.1 + 2.7.2

--- app/src/main/java/com/example/android/labwork_2_7_1/MainActivity.java
+++ app/src/main/java/com/example/android/labwork_2_7_1/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.android.labwork_2_7_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     // Имя файла
@@ -16,22 +23,61 @@ public class MainActivity extends AppCompatActivity {
     private static final String TEXT_SAVE_LOAD = "textValue";
     SharedPreferences sPref;
 
-    EditText textSaveLoad;
-    Button btnSave, btnLoad;
+    private EditText textSaveLoad;
+    private Button btnSavePref, btnLoadPref;
+
+    private Button btnSaveInternalStorage, btnLoadInternalStorage;
+    private static final String FILE_NAME = "fileName";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * Internal Storage
+         */
+        btnSaveInternalStorage = findViewById(R.id.button_save_internal);
+        btnLoadInternalStorage = findViewById(R.id.button_load_internal);
+
+        btnSaveInternalStorage.setOnClickListener(v -> {
+            String textSave = textSaveLoad.getText().toString();
+            if (textSave.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Ввдедите текст",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                try {
+                    saveInternalStorage(textSave);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                textSaveLoad.setText("");// очищаем TextView
+            }
+        });
+
+        btnLoadInternalStorage.setOnClickListener(v -> {
+            try {
+                textSaveLoad.setText(loadInternalStorage());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        /**
+         *  End Internal Storage
+         */
+
+
+        /**
+         * SharedPreferences
+         */
         textSaveLoad = findViewById(R.id.text_save_load);
-        btnSave = findViewById(R.id.button_save);
-        btnLoad = findViewById(R.id.button_load);
+        btnSavePref = findViewById(R.id.button_save);
+        btnLoadPref = findViewById(R.id.button_load);
         // Инициализируем
         sPref = getSharedPreferences(MY_PREF, Activity.MODE_PRIVATE);
 
         // SAVE
-        btnSave.setOnClickListener(v -> {
+        btnSavePref.setOnClickListener(v -> {
             String textSave = textSaveLoad.getText().toString();
             if (textSave.isEmpty()) {
                 Toast.makeText(MainActivity.this, "Ввдедите текст",
@@ -43,10 +89,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // LOAD
-        btnLoad.setOnClickListener(v -> textSaveLoad.setText(loadPreferences()));
+        btnLoadPref.setOnClickListener(v -> textSaveLoad.setText(loadPreferences()));
+        /**
+         * END SharedPreferences
+         */
 
     }
 
+    /**
+     * SharedPreferences
+     */
     // SAVE
     protected void savePreferences(String str) {
         SharedPreferences.Editor editor = sPref.edit();
@@ -60,6 +112,38 @@ public class MainActivity extends AppCompatActivity {
     public String loadPreferences() {
         return sPref.getString(TEXT_SAVE_LOAD, "");
     }
+    /**
+    * END SharedPreferences
+    */
 
+
+    /**
+     * Internal Storage
+     */
+    private void saveInternalStorage(String str) throws IOException {
+        // Открываем поток
+        FileOutputStream out = openFileOutput(FILE_NAME, MODE_PRIVATE);
+        out.write(str.getBytes());
+        out.close();
+        Toast.makeText(MainActivity.this, "Информация сохранена в файл: " +
+                        FILE_NAME,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public String loadInternalStorage() throws IOException {
+        // Открываем поток
+        FileInputStream in =  openFileInput(FILE_NAME);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringBuilder strL = new StringBuilder();
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            strL.append(str);
+        }
+        return strL.toString();
+    }
+    /**
+     *  End Internal Storage
+     */
 
 }
